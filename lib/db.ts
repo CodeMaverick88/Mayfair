5f11e8f8a96dc5eae168c9d8e426a7b70c9253ac
CodeMaverick88 <meisonramsay@gmail.com>
2026-06-18 01:18:05 +0300

    Prisma 2

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
+export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
-    // In Prisma 7, we pass the URL here instead of the schema
-    datasource: {
-      url: process.env.DATABASE_URL,
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
     },
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
