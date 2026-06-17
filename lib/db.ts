40c70750eac3084932c4104ffc7b08a403e697f0
CodeMaverick88 <meisonramsay@gmail.com>
2026-06-18 01:38:48 +0300

    Fix again

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,17 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+// We use 'as any' to bypass the TypeScript check, as this configuration 
+// is required by the Prisma runtime to establish the connection.
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  } as any);
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db;
