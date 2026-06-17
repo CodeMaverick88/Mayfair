58451ca1fb358a361ac38939ff052294f7b5542a
CodeMaverick88 <meisonramsay@gmail.com>
2026-06-18 01:24:45 +0300

    Prisma Lib

--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+// We pass an empty object {} to satisfy the PrismaClientOptions requirement
 export const db =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({});
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db;
