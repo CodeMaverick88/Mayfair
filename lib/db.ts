57b1bf00324f3f192ebfaf5b622ac3ac3c7d188c
CodeMaverick88 <meisonramsay@gmail.com>
2026-06-18 01:20:20 +0300

    Prisma 3

--- lib/db.ts
+++ lib/db.ts
@@ -4,12 +4,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const db =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = db;
+}
