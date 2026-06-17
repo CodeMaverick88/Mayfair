9ea9784a076bb4c8017baa424d4f033215f56467
CodeMaverick88 <meisonramsay@gmail.com>
2026-06-18 01:30:14 +0300

    Msm woiii

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// We provide an explicit configuration object to satisfy Prisma 7 requirements.
-// 'log' is a common option that ensures the object is non-empty.
-export const db =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db;
